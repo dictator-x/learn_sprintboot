d54aecaf12a58aae4de6d5838675b2780a96db61
dictator-x <dictator.xwu@gmail.com>
2020-07-02 13:03:53 -0400

    custom viewresolver

--- src/main/java/com/learnspringboot/Application.java
+++ src/main/java/com/learnspringboot/Application.java
@@ -4,6 +4,10 @@ import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.web.servlet.ViewResolver;
+import org.springframework.web.servlet.View;
+import java.util.Locale;
+import org.springframework.context.annotation.Bean;
 
 @SpringBootApplication
 public class Application {
@@ -16,4 +20,16 @@ public class Application {
 		logger.trace("Hello World...................");
 	}
 
+	@Bean
+	public ViewResolver myViewResolver() {
+		return new MyViewResolver();
+	}
+
+	private statck class MyViewResolver implements ViewResolver {
+
+		@Override
+		public View resolveViewName(String viewName, Locale locale) throws Exception {
+			return null;
+		}
+	}
 }
