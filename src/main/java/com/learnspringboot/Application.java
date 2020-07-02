874424f5cacae5efff818ab905cd28a869f667ae
dictator-x <dictator.xwu@gmail.com>
2020-07-01 20:40:48 -0400

    introduce log4j2

--- src/main/java/com/learnspringboot/Application.java
+++ src/main/java/com/learnspringboot/Application.java
@@ -1,5 +1,7 @@
 package com.learnspringboot;
 
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
@@ -8,6 +10,8 @@ public class Application {
 
 	public static void main(String[] args) {
 		SpringApplication.run(Application.class, args);
+		Logger logger = LoggerFactory.getLogger(Application.class);
+		logger.info("Hello World...................");
 	}
 
 }
