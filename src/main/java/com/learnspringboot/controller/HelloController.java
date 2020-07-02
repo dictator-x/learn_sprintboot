96f51297d95c077e4df4068716976d6ca8ff0d19
dictator-x <dictator.xwu@gmail.com>
2020-07-02 12:48:23 -0400

    add thymeleaf demo

--- src/main/java/com/learnspringboot/controller/HelloController.java
+++ src/main/java/com/learnspringboot/controller/HelloController.java
@@ -1,5 +1,6 @@
 package com.learnspringboot.controller;
 
+import java.util.Arrays;
 import java.util.Map;
 import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.RequestMapping;
@@ -19,7 +20,8 @@ public class HelloController {
 
 	@RequestMapping("/success")
 	public String success(Map<String, Object> map) {
-		map.put("hello", "Good Morning");
+		map.put("hello", "<h1>Good Morning</h1>");
+		map.put("users", Arrays.asList("Kobe", "Jordan", "James"));
 		return "success";
 	}
 
