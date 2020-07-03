ed5b24244100ea4c767bebac4bf4c1a4ddd6a5fa
dictator-x <dictator.xwu@gmail.com>
2020-07-03 14:59:06 -0400

    add restful demo

--- src/main/java/com/learnspringboot/controller/EmployeeController.java
+++ src/main/java/com/learnspringboot/controller/EmployeeController.java
@@ -7,6 +7,10 @@ import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.PutMapping;
+import org.springframework.web.bind.annotation.DeleteMapping;
 
 @Controller
 public class EmployeeController {
@@ -21,4 +25,39 @@ public class EmployeeController {
 		return "employees";
 	}
 
+	@GetMapping("/emp")
+	public String addEmployeePage() {
+		//TODO: create employeepage html.
+		return "emp/add";
+	}
+
+	@PostMapping("/emp")
+	public String addEmployee(Employee employee) {
+		employeeDao.save(employee);
+		return "redirect:/emps";
+	}
+
+	@GetMapping("/emp/{id}")
+	public String toEditPage(
+		@PathVariable("id") Integer id,
+		Model model
+	)
+	{
+		Employee employee = employeeDao.get(id);
+		model.addAttribute("emp", employee);
+		//TODO: create html
+		return "emp/add";
+	}
+
+	@PutMapping("/emp")
+	public String updateEmployee(Employee employee) {
+		employeeDao.save(employee);
+		return "redirect:/emps";
+	}
+
+	@DeleteMapping("/emp/{id}")
+	public String deleteEmployee(@PathVariable("id") Integer id) {
+		employeeDao.delete(id);
+		return "redirect:/emps";
+	}
 }
